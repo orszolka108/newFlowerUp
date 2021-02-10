9ba1587985e9162bfadf90aacbc0903e341b4641
Urszula Duda <37809042+orszolka108@users.noreply.github.com>
2021-02-10 08:44:50 +0100

    firebase config moved to env

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBUjrrKTgeDM4o845-ov_kPPysTJIAanXs',
-  authDomain: 'newflowerup.firebaseapp.com',
-  projectId: 'newflowerup',
-  storageBucket: 'newflowerup.appspot.com',
-  messagingSenderId: '787091338195',
-  appId: '1:787091338195:web:789e65075df260cddcf526',
-  measurementId: 'G-78RGS69SR6',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: REACT_APP_AUTH_DOMAIN,
+  projectId: REACT_APP_PROJECT_ID,
+  storageBucket: REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
+  appId: REACT_APP_APP_ID,
+  measurementId: REACT_APP_MEASUREMENT_ID,
 }
 
 firebase.initializeApp(firebaseConfig)
